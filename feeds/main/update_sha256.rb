c8d768c62daf40875626ef333ecf1bc771cfe3cd
deepakshinde <deepak.shinde@coriolis.co.in>
2017-09-27 11:17:43 +0530

    Updated main.json with sha256

---
+++ feeds/main/update_sha256.rb
@@ -0,0 +1,20 @@
+require 'json'
+require 'digest'
+file = File.read('main.json')
+data_hash = JSON.parse(file)
+
+data_hash["sites"].each do |h|
+    if !h["templates"].nil?
+      h["templates"].each do |template|
+        print template
+        sha1 = Digest::SHA256.hexdigest(template["image"])
+        template["sha256"] = sha1
+      end
+    end
+end
+
+File.open('main.json', "w") do |f|
+f.write(JSON.pretty_generate(data_hash))
+end
+
+
